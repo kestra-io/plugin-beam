ed135d53c1c729be7cacd7f332da3d85bc198b13
François Delbrayelle <fdelbrayelle@gmail.com>
2026-01-14 10:04:46 +0100

    feat: introduce Apache Beam plugin (#5)
    * feat: introduce Apache Beam plugin
    
    * feat: use sdk python + flink runner
    
    * feat: use sdk python + spark runner
    
    * test: make sanity-checks standalone
    
    * refactor: extend AbstractExecScript for ns files
    
    * feat: use sdk python + flink runner
    
    * fix: code review feedback

---
+++ src/main/java/io/kestra/plugin/beam/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    categories = {PluginSubGroup.PluginCategory.TRANSFORMATION, PluginSubGroup.PluginCategory.BATCH, PluginSubGroup.PluginCategory.INGESTION},
+    description = "Run Apache Beam pipelines on Direct, Flink, Spark, or Dataflow runners.",
+    title = "Apache Beam"
+)
+package io.kestra.plugin.beam;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
